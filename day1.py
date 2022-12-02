63fccaa7e2fecdb0ed9cc6d0d6bf0893da9c6730
Onel Shina <oshina@gravitypayments.com>
2022-12-01 18:27:57 -0800

    add day 1 - part 2 solution

--- day1.py
+++ day1.py
@@ -3,6 +3,12 @@ import os.path
 with open(os.path.join('inputs', 'inputDay1.txt')) as f:
     calories = list(map(str.split, f.read().split('\n\n')))
 
-total_calories = [sum(map(int, c)) for c in calories]
-max_calories = max(total_calories)
+total_calories = sorted([sum(map(int, c)) for c in calories])
+
+# part 1 - find elf with max calories
+max_calories = total_calories[-1]
 print(max_calories)
+
+# part 2 - find 3 top elfs with most calories
+max_3_calories = sum(total_calories[-3:])
+print(max_3_calories)
